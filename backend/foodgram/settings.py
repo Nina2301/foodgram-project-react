df981995103bc8eb3d79b6693e70f27e14cce831
Нина Карлова <nina-karlova@yandex.ru>
2023-11-15 23:24:24 +0300

    Change nginx.conf

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
