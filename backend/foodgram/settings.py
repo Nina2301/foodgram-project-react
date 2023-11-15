bb6f2b346bc3f06a6898557e496b6b467f9e264d
Нина Карлова <nina-karlova@yandex.ru>
2023-11-15 16:34:20 +0300

    Change 8 docker_compose

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -131,8 +131,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
